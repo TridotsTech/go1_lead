68b7e67ffccff531dba9f429dbaf54d963ad4012
Tridotstech <info@tridotstech.com>
2024-12-17 14:11:41 +0000

    feat: made labels in titlecase foor dynamically generating fields from FB

--- go1_leads/public/js/crm_lead_override.js
+++ go1_leads/public/js/crm_lead_override.js
@@ -12,13 +12,17 @@ frappe.ui.form.on("CRM Lead", {
 function render_fields(frm) {
     data = JSON.parse(frm.doc.custom_fb_data)
     let type = "Data"
-    console.log(data)
+    // console.log(data)
     const wrapper = frm.fields_dict.custom_fb_field.$wrapper
     $('[data-fieldname="custom_section_break_fqfqo"] div[class="section-head"]').remove()
     $('[data-fieldname="custom_section_break_fqfqo"]').prepend(`<div class="section-head">Facebook Lead Info</div>`)
     $('[data-fieldname="custom_fb_field"]').attr("style","display: flex;width: 100%;flex-wrap: wrap;")
     $('[data-fieldname="__column_3"]').attr("style","padding:0px !important")
     for (let i of data[0].field_data) {
+        label = (i.name).split('_').map(function (word) {
+            return (word.charAt(0).toUpperCase() + word.slice(1));
+        }).join(' ');
+        fieldname = (i.name).split(' ').join('_').toLowerCase();
         if (i.values[0].length > 140) {
             type = "Small Text"
         }
@@ -26,12 +30,35 @@ function render_fields(frm) {
             parent: wrapper,
             df: {
                 "fieldtype": type,
-                "label": __(`${i.name}`),
-                "fieldname": `${i.name}`
+                "label": __(`${label}`),
+                "fieldname": `${fieldname}`
             },
             render_input: true
         })
-        $(`div[data-fieldname="${i.name}"]`).addClass('col-sm-4')
-        $(`[data-fieldname="${i.name}"] [data-fieldname="${i.name}`).prop("value",i.values[0])
+        $(`div[data-fieldname="${fieldname}"]`).addClass('col-sm-4')
+        $(`[data-fieldname="${fieldname}"] [data-fieldname="${fieldname}`).prop("value",i.values[0])
+    }
+    for(let [key,values] of Object.entries(data[0])){
+        if (key != "field_data"){
+            label = key.split('_').map(function (word) {
+                return (word.charAt(0).toUpperCase() + word.slice(1));
+            }).join(' ');
+            fieldname = key.split(' ').join('_').toLowerCase();
+            if (values.length > 140) {
+                type = "Small Text"
+            }
+            frappe.ui.form.make_control({
+                parent: wrapper,
+                df: {
+                    "fieldtype": type,
+                    "label": __(`${label}`),
+                    "fieldname": `${fieldname}`
+                },
+                render_input: true
+            })
+            $(`div[data-fieldname="${fieldname}"]`).addClass('col-sm-4')
+            $(`[data-fieldname="${fieldname}"] [data-fieldname="${fieldname}`).prop("value",values)
+        }
+        // console.log(key,values)
     }
 }
